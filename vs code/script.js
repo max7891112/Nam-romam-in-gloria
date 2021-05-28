d91ffbc72d9caa47f8f538b4c2555d8ede127268
Max <alkhimovich.elenka@mail.ru>
2021-05-28 19:20:18 +0300

    first commit

---
+++ vs code/script.js
@@ -0,0 +1,21 @@
+// "use strict";
+
+// // let answer = prompt("Есть ли вам 18?","да");
+// // console.log(typeof(answer));
+
+// // alert ("hello world");
+
+// let alfa = 10,
+//     decr = 10;
+// console.log(++alfa);
+// console.log(decr--);
+// Console.log("2" == 2);
+
+let a = 2;
+let b = 'hello';
+
+console.log(a += b); // addition
+// expected output: 5
+
+console.log(b += ' world'); // concatenation
+// expected output: "hello world"
