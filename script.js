21ccf306d1a07d9536477cda670f65f1d6e590eb
Max <alkhimovich.elenka@mail.ru>
2021-05-30 18:06:14 +0300

    communicate with mu user

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let a = 2;
-let b = 1;
-console.log(a + b);
+let money;
+let time;
+money = prompt ("Ваш бюджет на месяц?","100000");
+time = prompt("Введите дату", "YYYY-MM-DD");
+let appData = {
+budget: money,
+//  данные времени - 
+timeData: time,
+//  объект с обязательными расходами - 
+expenses: {},
+//  объект с необязательными расходами - 
+optionalExpenses:{"FirstQuestions" : "SecondQuestions"},
+//  массив данных с доп. доходом - 
+income:{}, 
+//  свойство 
+savings: false
+};
+
+let FirstQuestions = prompt("Введите обязательную статью расходов в этом месяце", "");
+let SecondQuestions = prompt("Во сколько обойдется?","");
+
+alert("Ваш бюджет на один день составляет " + appData.budget/30);
